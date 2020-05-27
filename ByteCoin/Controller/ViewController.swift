608146e752ac73635edf0b26347aa1242ad598d9
Demario Douce <demario.douce567@gmail.com>
2020-05-26 20:13:11 -0400

    added UIPickerView Functionality

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //UI Elements
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var currencyLbl: UILabel!
+    @IBOutlet weak var bitcoinLbl: UILabel!
+    
+    //Object
+    var coinManagerRef = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
     }
 
 
+}
+
+//MARK: - UIPickerViewDataSource
+extension ViewController: UIPickerViewDataSource {
+    
+func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    
+    return 1
+}
+
+func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    
+    return coinManagerRef.currencyArray.count
+}
+    
+}
+
+//MARK: - UIPickerViewDelegate
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return coinManagerRef.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let currencySelected = coinManagerRef.currencyArray[row]
+        
+        coinManagerRef.getCoinPrice(for: currencySelected)
+    }
+    
 }
 
